f2b6673d28584c749f21db8f2b31e1b0591c51a7
kurabyo <bladvazovski@gmail.com>
2023-02-03 15:15:46 +0200

    filter(sort) feature realesed

--- src/components/Content/Meals/Meals.jsx
+++ src/components/Content/Meals/Meals.jsx
@@ -5,27 +5,48 @@ import MealsList from './MealsLIst/MealsList';
 import './Meals.css'
 
 function Meals() {
+  const options = [
+    'All', 'Can be cooked', 'Michaeldog'
+  ];
+
   const [data, setData] = useState();
+  const [dataMap, setDataMap] = useState([])
+  const [sort, setSort] = useState(options[0]);
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
         'https://63dbfdc1a3ac95cec5af619c.mockapi.io/cook/meals',
       );
-
-      setData(result.data);
+      setData(result.data)
+      setDataMap(result.data)
     };
-
     fetchData();
   }, []);
 
-  console.log(data)
+  useEffect(() => {
+    switch (sort) {
+      case options[0]:
+        setDataMap(prev => data)
+        break;
+      case options[1]:
+        setDataMap(prev => prev.filter(e => e.can === true))
+        break
+      case options[2]:
+        setDataMap(prev => [])
+        break
+    
+      default:
+        break;
+    }
+    
+  }, [sort]);
 
   return (
     <div>
-        <Sort />
+        <Sort options={options} sort={sort} set={setSort}/>
         <div className='meals_grid'>
-          {data?.map(e => <MealsList key={e.id} img={e.img} title={e.name}/>)}
+          {dataMap?.map(e => <MealsList key={e.id} img={e.img} title={e.name}/>)}
         </div>
     </div>
   )
--- src/components/Content/Meals/Sort/Sort.jsx
+++ src/components/Content/Meals/Sort/Sort.jsx
@@ -3,16 +3,23 @@ import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import s from './Sort.module.css'
 
-function Sort() {
-    const options = [
-        'All', 'Can be cooked', 'Michaeldog'
-    ];
+function Sort(param) {
 
   return (
     <div className={s.sortbar}>
-        <Dropdown options={options} className={s.root} controlClassName={s.control} placeholderClassName={s.placeholder} menuClassName={s.menu}/>
+      <Dropdown
+        onChange={(e) => {
+          param.set(e.value);
+        }}
+        value={param.options[0]}
+        options={param.options}
+        className={s.root}
+        controlClassName={s.control}
+        placeholderClassName={s.placeholder}
+        menuClassName={s.menu}
+      />
     </div>
-  )
+  );
 }
 
 export default Sort
