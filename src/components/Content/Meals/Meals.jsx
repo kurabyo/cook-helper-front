100bcce68660aecf98c7a96848ba0a879289ec26
kurabyo <bladvazovski@gmail.com>
2023-02-07 17:12:19 +0200

    meals sorting by can/feature

--- src/components/Content/Meals/Meals.jsx
+++ src/components/Content/Meals/Meals.jsx
@@ -1,33 +1,67 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Sort from './Sort/Sort'
 import MealsList from './MealsLIst/MealsList';
 import './Meals.css'
 import { Link } from 'react-router-dom';
+import AuthContext from '../../../context/AuthContext';
+import { API } from '../../../utils/useAxios';
 
 function Meals({ data }) {
-
   const options = [
     'All', 'Can be cooked', 
   ];
 
+  const { user } = useContext(AuthContext)
+
+  const [userStor, setUserStor] = useState([]);
+  const [optionS, setOptionS] = useState(options[0]);
+
+  const userIngedients = userStor.map(e => e.ingredient_id)
+
+  const getStorage = async () => {
+    await API.get("user_storages/")
+      .then((res) => {
+        setUserStor(res.data?.filter(({ user_id }) => user_id === user.user_id));
+      })
+      .catch(console.error);
+  }
 
   
 
+  useEffect(() => {
+    getStorage()
+
+  }, [])
+
+  const handleOptionChange = (e) => {
+    setOptionS(e.value)
+  }
 
 
-  return ( 
-    <div>
-      <Sort options={options} />
+  console.log(userStor)
+  console.log(userIngedients)
+  console.log(data)
+
+  return (
+    <div className="main">
+      <Sort options={options} handleFunc={handleOptionChange} />
       <div className="meals_grid">
-        {data?.map((e) => (            
-            <Link className='link_item' key={e.id} to={e.id.toString()}>
-              <MealsList
-                obj={e}
-                key={e.id}
-                title={e.name}
-              />
+        {options[0] === optionS &&
+          data?.map((e) => (
+            <Link className="link_item" key={e.id} to={e.id.toString()}>
+              <MealsList obj={e} key={e.id} title={e.name} />
             </Link>
-        ))}
+          ))}
+        {options[1] === optionS &&
+          data
+            ?.filter((e) => {
+              return e.ingredients.every(o => userIngedients.includes(o));
+            })
+            ?.map((e) => (
+              <Link className="link_item" key={e.id} to={e.id.toString()}>
+                <MealsList obj={e} key={e.id} title={e.name} />
+              </Link>
+            ))}
       </div>
     </div>
   );
--- src/components/Content/Meals/MealsLIst/MealsList.jsx
+++ src/components/Content/Meals/MealsLIst/MealsList.jsx
@@ -1,30 +1,11 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import s from "./MealsList.module.css";
 
 export default function MealsList(props) {
-  const [blobImg, setBlobImg] = useState(null);
-
-  console.log(props)
-
-  // useEffect(() => {
-  //   fetch(props.obj?.img)
-  //   //                         vvvv
-  //   .then(response => console.log(response))
-  //   // .then(imageBlob => {
-  //   //     // Then create a local URL for that image and print it 
-  //   //    setBlobImg(URL.createObjectURL(imageBlob))
-  //   //     console.log(blobImg);
-  //   //     console.log(imageBlob);
-        
-        
-  //   // });
-
-  // }, [])
-
 
   return (
     <div className={s.card}>
-      <img className={s.card_image} src={blobImg} alt="Logo" />
+      <img className={s.card_image} src='#' alt="Logo" />
       <div className={s.card_header}>
           <h5 className={s.card_title}>{props.title}</h5>
       </div>  
